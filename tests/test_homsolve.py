330307ef04c68b13f24de76b58ec3c0b5c9c8d3c
seibelsh <28317696+seibelsh@users.noreply.github.com>
2020-12-16 18:54:27 +0100

    Update test_homsolve.py

--- tests/test_homsolve.py
+++ tests/test_homsolve.py
@@ -1,1 +1,11 @@
+"""Test homsolve function.
 
+
+HomCont:
+Solving systems of nonlinear equations by homotopy continuation.
+
+Copyright (C) 2018  Steffen Eibelshäuser & David Poensgen
+
+This program is free software: you can redistribute it
+and/or modify it under the terms of the MIT License.
+"""
